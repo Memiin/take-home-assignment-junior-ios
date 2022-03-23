8b944242c8b4479b3bf3c59fbd37c3bb13d6dcde
Guillermo Jimenez <guillermo.jimenez@ooqia.com>
2022-03-22 18:50:17 -0600

    SwiftUI project creation

---
+++ swiftui_test/swiftui_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_test
+//
+//  Created by Guillermo Jimenez on 22/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_test/swiftui_test/swiftui_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_testApp.swift
+//  swiftui_test
+//
+//  Created by Guillermo Jimenez on 22/3/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
